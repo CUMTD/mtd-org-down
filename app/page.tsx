1c7056f280b666f02073a7eb15ae8563805b2a69
Ryan Blackman <rblackman@mtd.org>
2023-07-03 12:52:36 -0500

    Update with Amy's copy.

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,12 @@ export default function Home() {
 			<main>
 				<h1>MTD.org is Under Maintenance</h1>
 				<p>
-					MTD.org is undergoing maintenance and is currently unavailable.
-					During this maintenance, realtime information is unavailable.
+					Apologies for the inconvenience, but MTD.org is undergoing critical maintenance and is currently unavailable.
+					Real-time departure information is not available during this update.
 					Please check <Link href="https://www.google.com/maps/dir/data=!3m1!4b1!4m5!4m4!1m1!4e1!1m0!3e3?entry=ttu" prefetch={true}>Google Maps</Link> or the <Link href="https://transitapp.com/">Transit App</Link> for <em>scheduled</em> information.
 				</p>
 				<p>
+					We'll be back with you soon. 
 					Follow us on social media for the latest updates.
 				</p>
 			</main>
